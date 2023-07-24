3d8e5e60247384aefbfdb5f3b78fc9f5a4071f42
ollyjw <owilcocks@gmail.com>
2023-07-24 15:28:51 +0100

    added knightMoves function page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 // import './chess-knight.png';
+import './styles.css';
+import { knightMoves } from './knightMoves';
 
 // A knight in chess can move to any square on the standard 8x8 chess board from any other square on the board, given enough turns. Its basic move is two steps forward and one step to the side.
 
@@ -12,110 +14,9 @@
 
 // Use Breadth first search - the chessboard is most accurately represented as a 64-node graph. It's not really a tree since there are multiple ways to reach any particular node.
 
-const board = genBoard();
 
-class Knight {
-    constructor(row, col) {
-        this.row = row;
-        this.col = col;
-    }
-    getCoord() {
-        return board[this.row][this.col];
-    }
-    getPossibleMoves() { //two steps forward and one step to the side.
-        // Make 2 arrays, one containing all possible moves from a given coord (calculations of row/col indeces) & another to store the new coords
-        const result = [];
-        const moves = [
-            [
-                this.row - 2,
-                this.col - 1,
-            ],
-            [
-                this.row - 2,
-                this.col + 1,
-            ],
-            [
-                this.row - 1,
-                this.col - 2,
-            ],
-            [
-                this.row - 1,
-                this.col + 2,
-            ],
-            [
-                this.row + 2,
-                this.col - 1,
-            ],
-            [
-                this.row + 2,
-                this.col + 1,
-            ],
-            [
-                this.row + 1,
-                this.col - 2,
-            ],
-            [
-                this.row + 1,
-                this.col + 2,
-            ],
-        ];
-        moves.forEach(possibleMove => {
-            // for each item in moves array check possible move coords are within boundaries
-            if (isWithinBoard(...possibleMove)) {
-                // then create new knight node, add these coordinates to it's row & col properties and add each knight node to result array
-                result.push(new Knight(...possibleMove));
-            }
-        })
-        return result;
-    }    
-}
+// const testKnight = new Knight(3,3); //5D
+// console.log(testKnight);
+// console.log(testKnight.getPossibleMoves());
 
-// Generate board and assign co-ordinates to each square
-function genBoard() {
-    const container = document.querySelector('.container');
-    const gameboard = document.createElement('div');
-    const chessBoard = [];
-    gameboard.classList.add('gameboard');
-    for (let i = 8; i > 0; i--) { // ROW i.e nums
-        // gen 8 row arrays each containing each coord in row
-        const rowArray = [];
-        let rowCoord = i;
-        for (let j = 0; j < 8; j++) { // COL i.e. letters
-            const colCoord = String.fromCharCode(65 + j); // convert to letters;
-            const coordinate = `${rowCoord}${colCoord}`;
-            const square = document.createElement('div');
-            square.id = coordinate;
-            square.textContent = coordinate;
-            rowArray.push(coordinate);
-            square.classList.add('square');
-            gameboard.appendChild(square);
-
-            if ((i + j) % 2 == 0) {
-                square.classList.add('square-black');
-            } else {
-                square.classList.add('square-white');
-            }
-        }        
-        chessBoard.push(rowArray); // access coords by [rowIndex][colIndex]
-    }
-    console.log(chessBoard);
-    console.log(chessBoard[5][2]);
-    container.appendChild(gameboard);
-    return chessBoard;
-}
-
-function isWithinBoard(i, j) {
-    return (i >= 0 && i <= 7 && j >= 0 && j <= 7);
-}
-
-// Use the chosen search algorithm to find the shortest path between the starting square (or node) and the ending square. Output what that full path looks like, e.g.:
-//   > knightMoves([3,3],[4,3])
-//   => You made it in 3 moves!  Here's your path:
-//     [3,3]
-//     [4,5]
-//     [2,4]
-//     [4,3]
-
-function knightMoves() {
-
-}
+console.log(`Knight moves from 5D to 4D will be: ${knightMoves([3,3],[4,3])}`);
---
+++ src/knightMoves.js
@@ -0,0 +1,122 @@
+import { genBoard, displayBoard } from './board';
+
+export const board = genBoard();
+displayBoard();
+
+class Knight {
+    constructor(row, col) {
+        this.row = row;
+        this.col = col;
+    }
+    getCoord() {
+        return board[this.row][this.col];
+    }
+    getPossibleMoves() { //two steps forward and one step to the side.
+        // Make 2 arrays, one containing all possible moves from a given coord (calculations of row/col indeces) & another to store the new coords
+        const result = [];
+        const moves = [
+            [
+                this.row - 2,
+                this.col - 1,
+            ],
+            [
+                this.row - 2,
+                this.col + 1,
+            ],
+            [
+                this.row - 1,
+                this.col - 2,
+            ],
+            [
+                this.row - 1,
+                this.col + 2,
+            ],
+            [
+                this.row + 2,
+                this.col - 1,
+            ],
+            [
+                this.row + 2,
+                this.col + 1,
+            ],
+            [
+                this.row + 1,
+                this.col - 2,
+            ],
+            [
+                this.row + 1,
+                this.col + 2,
+            ],
+        ];
+        moves.forEach(possibleMove => {
+            // for each item in moves array check possible move coords are within boundaries
+            if (isWithinBoard(...possibleMove)) {
+                // then create new knight node, add these coordinates to it's row & col properties and add each knight node to result array
+                result.push(new Knight(...possibleMove));
+            }
+        })
+        return result;
+    }    
+}
+
+function isWithinBoard(i, j) {
+    return (i >= 0 && i <= 7 && j >= 0 && j <= 7);
+}
+
+
+// Use the chosen search algorithm to find the shortest path between the starting square (or node) and the ending square. Output what that full path looks like, e.g.:
+//   > knightMoves([3,3],[4,3])
+//   => You made it in 3 moves!  Here's your path:
+//     [3,3]
+//     [4,5]
+//     [2,4]
+//     [4,3]
+
+export function knightMoves(currentLocation, destination) {    
+    // Create queue that will keep track of what nodes need to be searched
+    const queueArray = [];
+    const levelOrderArray = [];    
+    // set first el to knight node's given current location
+    queueArray.push(new Knight(...currentLocation));
+
+    // create a while loop that will run as long as the queue is not empty. In other words, it will run as long we still have nodes to traverse.
+    // while the currentLocation & destination coords aren't equal - queueArray items are knights, destin items will be xy coords
+    while ((queueArray[0].row !== destination[0]) || (queueArray[0].col !== destination[1])) {
+        //we need to remove the first knight node in the queue and store in 'currentKnight'
+        const currentKnight = queueArray.shift();
+        // Store knight's possible moves in var
+        const possibleMoves = currentKnight.getPossibleMoves(); // 8 poss knight objs
+        
+        // Take the current knight, loop through all possible moves on the board it can make and add parent prop to it's children - as new current knights are created from possibles moves they will be assigned parent prop linking to their parent's coords
+        possibleMoves.forEach(possibleMove => {
+            possibleMove.parent = currentKnight;
+        })
+
+        //console.log(currentKnight);
+
+        // Add possible moves (knight objs) to queue
+        queueArray.push(...possibleMoves);
+        
+        // we need to make sure the current node is not null - If the node is null, we know we've reached the end of a branch
+        if (currentKnight === null) continue;
+            // (continue statement terminates execution of the statements in the current iteration of the current or labeled loop, and continues execution of the loop with the next iteration).
+    } 
+console.log(queueArray);
+    // Remove & store first el from queue array
+    let knightDestination = queueArray.shift();
+    console.log(knightDestination);
+    let counter = 0;
+    // While knights have a parent property
+    while(knightDestination.parent) {
+        // add knights props to front of LO array
+        levelOrderArray.unshift([knightDestination.row, knightDestination.col]);
+        // Assign each knight the values of it's parent property 
+        knightDestination = knightDestination.parent;
+        console.log(`LO ARRAY: ${levelOrderArray}`);
+        counter ++;
+    }
+    console.log(`You made it in ${counter} moves!`);
+    // add knights props to array outside of loop to return currentLocation
+    levelOrderArray.unshift([knightDestination.row, knightDestination.col]);
+    return levelOrderArray; // this currently returns one array, rather than array of separate coord arrays...
+}
